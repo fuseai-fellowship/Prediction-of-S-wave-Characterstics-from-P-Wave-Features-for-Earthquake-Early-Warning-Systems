27c6115506e41ef070accc315d64b1cef1e9c46d
Tunisha <gyawalitunisha1@gmail.com>
2025-10-30 21:37:48 +0545

    Delete src directory

--- src/models/evaluate_model.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
-
-def regression_metrics(y_true, y_pred):
-    return {
-        'R2': float(r2_score(y_true, y_pred)),
-        'MAE': float(mean_absolute_error(y_true, y_pred)),
-        'RMSE': float(np.sqrt(mean_squared_error(y_true, y_pred)))
-    }
